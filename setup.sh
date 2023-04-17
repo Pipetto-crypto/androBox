dc3678713b9f7328aa7df700c3578d558452c051
Pipetto-crypto <Pipetto-crypto&github.com>
2023-04-17 21:22:32 +0200

    ubuntu: Add update function to ubuntu

--- setup.sh
+++ setup.sh
@@ -3,10 +3,17 @@
 echo -e "\nInstalling required dependencies"
 
 pkg update && pkg upgrade
-pkg install pulseaudio virglrenderer-android mesa xorg-server-xvfb xwayland
+pkg install x11-repo -y
+pkg install pulseaudio virglrenderer-android mesa xorg-server-xvfb xwayland megadl proot-distro -y
+
+if [ ! -d ~/storage ]
+then
+     echo -e "\nGranting internal storage permissions"
+fi
 
 echo -e "\nInstalling the rootfs"
 
+mkdir -p $PREFIX/var/lib/proot-distro && mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs
 proot-distro restore ubuntu.tar.gz
-wget 
+wget https://raw.githubusercontent.com/Pipetto-crypto/3in1ProotX86Emus/master/ubuntu
 chmod +x ubuntu && mv ubuntu $PREFIX/bin
