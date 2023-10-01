677a2e82ddabece52514b70d317e702ede0ad94f
Pipetto-crypto <Pipetto-crypto&github.com>
2023-10-01 12:55:44 +0200

    Finishing last changes for project transition

--- setup.sh
+++ setup.sh
@@ -5,21 +5,22 @@ echo -e "\nInstalling required dependencies"
 termux-change-repo
 pkg update && pkg upgrade
 pkg install x11-repo -y
-pkg install pulseaudio virglrenderer-android mesa xorg-server-xvfb xwayland wget proot-distro -y
+pkg install pulseaudio virglrenderer-android mesa wget proot-distro -y
 
 if [ ! -d ~/storage ]
 then
      echo -e "\nGranting internal storage permissions"
 fi
 
-mkdir -p $PREFIX/var/lib/proot-distro && mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs
+mkdir -p $PREFIX/var/lib/androBox
 if [ -f /sdcard/ubuntu.tar.gz ]
 then
      echo -e "\nInstalling Ubuntu rootfs" 
-     proot-distro restore /sdcard/ubuntu.tar.gz
+     tar -xvzf /sdcard/ubuntu.tar.gz -C $PREFIX/var/lib/androBox
 else 
      echo -e "\nNo rootfs detected, leaving"
      exit
 fi
-wget https://raw.githubusercontent.com/Pipetto-crypto/3in1ProotX86Emus/master/ubuntu
-chmod +x ubuntu && mv ubuntu $PREFIX/bin
+
+wget https://raw.githubusercontent.com/Pipetto-crypto/androBox/master/androBox
+chmod +x androBox && mv androBox $PREFIX/bin
