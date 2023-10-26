0db91060b4e425df44e2afc9d9b81133b472db72
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-10-26 20:04:47 +0200

    configs: update new configs and switch to new project

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 echo -e "\nInstalling required dependencies"
 
 termux-change-repo
-pkg update && pkg upgrade
+pkg update && pkg upgrade -y
 pkg install x11-repo tur-repo -y
-pkg install pulseaudio git virglrenderer-android mesa wget fontconfig freetype libpng termux-x11-nightly cabextract zenity -y
+pkg install pulseaudio git virglrenderer-android mesa wget fontconfig freetype libpng termux-x11-nightly cabextract zenity openbox file xorg-xrandr xterm -y
 
 if [ ! -d ~/storage ]
 then
@@ -16,27 +16,39 @@ fi
 if [ -f /sdcard/glibc_prefix.tar.xz ]
 then
      echo -e "\nInstalling the glibc prefix" 
-     tar -xvzf /sdcard/glibc_prefix.tar.xz -C $PREFIX
+     tar -xvf /sdcard/glibc_prefix.tar.xz -C $PREFIX
 else 
      echo -e "\nNo glibc prefix detected, put the prefix into the root of your sdcard"
      exit
 fi
 
-if [ -f /sdcard/wine-8.14-amd64.tar.xz ]
+if [ -f /sdcard/wine-*-amd64.tar.xz ]
 then
      echo -e "\nInstalling patched wine"
-     tar -xvf /sdcard/wine-8.14-amd64.tar.xz -C $PREFIX/opt
+     tar -xvf /sdcard/wine-8.14-amd64.tar.xz -C $PREFIX/glibc/opt
 else
      echo -e "\nNo patched wine detected, put the patched wine into the root of your sdcard"
      exit
 fi
 
-git clone https://github.com/Pipetto-crypto/androBox.git -b androBoxNew $HOME
+git clone https://github.com/Pipetto-crypto/androBox.git -b androBoxNew 
 for item in $HOME/androBox/scripts/*
 do
    [[ ! -d $item ]] && chmod +x $item && mv $item $PREFIX/bin
 done
 
+mkdir -p /sdcard/androBox
+mv $HOME/androBox/configs /sdcard/androBox
+
+echo "check_certificate = off" > $HOME/.wgetrc
+
 rm -rf $HOME/androBox
+wine wineboot
+pfxupdate
 
+cat >> $HOME/.androBox <<- EOM
+#androBox configuration file
 
+checkres=enabled
+services=disabled
+EOM
