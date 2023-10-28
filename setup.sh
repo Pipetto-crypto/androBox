1c33ce443ae1f945df9f3e943d735a2e29215573
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-10-28 09:39:49 +0200

    setup: completed setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo -e "\nInstalling required dependencies"
 termux-change-repo
 pkg update && pkg upgrade -y
 pkg install x11-repo tur-repo -y
-pkg install pulseaudio git virglrenderer-android mesa wget fontconfig freetype libpng termux-x11-nightly cabextract zenity openbox file xorg-xrandr xterm -y 
+pkg install pulseaudio git virglrenderer-android mesa wget fontconfig freetype libpng termux-x11-nightly cabextract zenity openbox file xorg-xrandr xterm iconv -y 
 
 if [ ! -d ~/storage ]
 then
