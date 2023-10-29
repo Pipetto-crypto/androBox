37498911f602861b809e15b8055c157d45a8a45b
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-10-29 06:36:59 +0100

    setup: improved setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo -e "\nInstalling required dependencies"
 
 termux-change-repo
-pkg update && pkg upgrade -y
+pkg upgrade -y
 pkg install x11-repo tur-repo -y
 pkg install pulseaudio git virglrenderer-android mesa wget fontconfig freetype libpng termux-x11-nightly cabextract zenity openbox file xorg-xrandr xterm iconv termux-exec -y 
 
@@ -14,8 +14,7 @@ then
      sleep 25
 fi
 
-wget -nc https://github.com/Pipetto-crypto/androBox/releases/download/glibc_prefix/glibc_prefix.tar.xz -P /sdcard
-[[ -d $PREFIX/glibc ]] && mv $PREFIX/glibc $PREFIX/glibc1
+[[ ! -f /sdcard/glibc_prefix.tar.xz ]] && wget https://github.com/Pipetto-crypto/androBox/releases/download/glibc_prefix/glibc_prefix.tar.xz -P /sdcard
 tar -xvf /sdcard/glibc_prefix.tar.xz -C $PREFIX
 
 echo -e "
@@ -26,12 +25,12 @@ read -p "Select a wine version to install:" installchoice
 
 case $installchoice in
 1) 
-    wget -nc https://github.com/Pipetto-crypto/androBox/releases/download/wine-8.0/wine-8.0-amd64.tar.xz -P /sdcard
+    [[ ! -f /sdcard/wine-8.0-amd64.tar.xz ]] && wget https://github.com/Pipetto-crypto/androBox/releases/download/wine-8.0/wine-8.0-amd64.tar.xz -P /sdcard
     tar -xvf /sdcard/wine-8.0-amd64.tar.xz -C $PREFIX/glibc/opt
     ;;
 2)
-    wget -nc https://github.com/Pipetto-crypto/androBox/releases/download/wine/wine-8.14-amd64.tar.xz -P 
-    tar -xvf /sdcard/wine-8.0-amd64.tar.xz -C $PREFIX/glibc/opt
+    [[ ! -f /sdcard/wine-8.14-amd64.tar.xz ]] && wget https://github.com/Pipetto-crypto/androBox/releases/download/wine/wine-8.14-amd64.tar.xz -P /sdcard
+    tar -xvf /sdcard/wine-8.14-amd64.tar.xz -C $PREFIX/glibc/opt
     ;;
 esac
 
