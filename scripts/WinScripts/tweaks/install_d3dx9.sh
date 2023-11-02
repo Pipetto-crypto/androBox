8bd531707ad1988ed58151925eb8622720775e82
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-02 12:43:23 +0100

    scripts: Second attempt at fixing winetricks and tweaks

--- scripts/WinScripts/tweaks/install_d3dx9.sh
+++ scripts/WinScripts/tweaks/install_d3dx9.sh
@@ -4,3 +4,4 @@ export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
 xterm -e $PREFIX/glibc/opt/WinScripts/winetricks.sh d3dx9
+winedesktop >/dev/null 2>&1 &
--- scripts/WinScripts/tweaks/install_openal.sh
+++ scripts/WinScripts/tweaks/install_openal.sh
@@ -4,3 +4,4 @@ export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
 xterm -e $PREFIX/glibc/opt/WinScripts/winetricks.sh openal
+winedesktop >/dev/null 2>&1 &
