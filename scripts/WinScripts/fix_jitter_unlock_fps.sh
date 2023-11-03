44ba22e5ee984fd510da696188dcb38f144ec609
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-03 09:26:23 +0100

    scripts: repair fix jitter script

--- scripts/WinScripts/fix_jitter_unlock_fps.sh
+++ scripts/WinScripts/fix_jitter_unlock_fps.sh
@@ -3,5 +3,5 @@
 export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
-sed -i "s/fifo/mailbox/g" /sdcard/uservars.sh
+sed -i "s/fifo/mailbox/g" /sdcard/androBox/uservars
 $PREFIX/glibc/opt/WinScripts/restart.sh
