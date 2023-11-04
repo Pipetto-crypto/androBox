cbc49946e98df47bfc3a2a6545a98e2fd9a921c3
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-04 19:56:47 +0100

    scripts: fix missing drivers when services are disabled

--- scripts/WinScripts/startup.bat
+++ scripts/WinScripts/startup.bat
@@ -3,3 +3,5 @@
 start /B F:\ProcessLasso\ProcessGovernor.exe
 start /B F:\7zip\7zFM.exe E:
 start /B Z:\usr\glibc\opt\WinScripts\ProcessLasso.bat
+findstr /c:"services=disabled" Z:\home\.androBox >nul 2>&1
+if not errorlevel 1 taskkill /im services.exe /f
