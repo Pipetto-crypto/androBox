d004f903581928a0a9663074e60a92fa0b5e50ae
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-04 08:20:12 +0100

    scripts: Add Windows Media Player 9 tweak installation

--- scripts/WinScripts/tweaks/install_wmp9.sh
+++ scripts/WinScripts/tweaks/install_wmp9.sh
@@ -3,5 +3,5 @@
 export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
-xterm -e $PREFIX/glibc/opt/WinScripts/winetricks.sh wmp11
+xterm -e $PREFIX/glibc/opt/WinScripts/winetricks.sh wmp9
 $PREFIX/glibc/opt/WinScripts/restart.sh
