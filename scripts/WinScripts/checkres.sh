4b0706b8b66f5db89bdbba8aad112f84f1f8fc86
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-05 15:22:31 +0100

    scripts: another hotfix

--- scripts/WinScripts/checkres.sh
+++ scripts/WinScripts/checkres.sh
@@ -3,7 +3,15 @@
 export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
-export DISPLAY=:0 
+export DISPLAY=${DISPLAY:-:1}
+
+if [ "$(ps -aux | grep -w '[:]1' | awk '{print $2}')" == "" ]
+then
+     kill $(pidof app_process)
+     termux-x11 :1 >/dev/null 2>&1 &
+     sleep 7
+fi
+
 watch -g 'xrandr | grep current | awk "{print \$8\$9\$10}" | tr -d ","' >/dev/null &>/dev/null
 until [ "$(ps -aux | grep '[w]atch' | awk '{print $2}')" == "" ]
 do 
