c3c89bc18f20c984f38c41d77f8f3df8c52e6f7a
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-06 16:29:03 +0100

    scripts: hotfix to winedesktop function

--- scripts/WinScripts/checkres.sh
+++ scripts/WinScripts/checkres.sh
@@ -5,12 +5,6 @@ unset LD_LIBRARY_PATH
 
 export DISPLAY=${DISPLAY:-:1}
 
-if [ "$(ps -aux | grep -w '[:]1' | awk '{print $2}')" == "" ]
-then
-     kill $(pidof app_process)
-     termux-x11 :1 >/dev/null 2>&1 &
-     sleep 7
-fi
 
 watch -g 'xrandr | grep current | awk "{print \$8\$9\$10}" | tr -d ","' >/dev/null &>/dev/null
 until [ "$(ps -aux | grep '[w]atch' | awk '{print $2}')" == "" ]
