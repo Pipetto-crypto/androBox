d59122db4de3d6a251b78b799a33f60f54716cb2
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-06 08:40:12 +0100

    scripts: switch to openal installer

--- scripts/WinScripts/tweaks/install_openal.bat
+++ scripts/WinScripts/tweaks/install_openal.bat
@@ -1,3 +1,5 @@
 @echo off
 
-start /W /unix /data/data/com.termux/files/usr/glibc/opt/WinScripts/tweaks/install_openal.sh
+start /W F:\oalinst.exe
+reg add HKEY_CURRENT_USER\Software\Wine /v DllOverrides /f
+reg add HKEY_CURRENT_USER\Software\Wine\DllOverrides /v openal32 /t REG_SZ /d native,builtin /f
