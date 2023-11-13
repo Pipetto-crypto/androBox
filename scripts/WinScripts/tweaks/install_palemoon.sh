39c629ad5995143c14349c79e7e45252a3cde3aa
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-13 21:16:59 +0100

    scripts: Add Palemoon web browser

---
+++ scripts/WinScripts/tweaks/install_palemoon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export PATH=/data/data/com.termux/files/usr/bin
+unset LD_LIBRARY_PATH
+
+cachedir=~/.local/cache
+
+mkdir -p $cachedir
+
+wget https://rm-eu.palemoon.org/release/palemoon-32.5.0.win64.installer.exe -P $cachedir
+wine $cachedir/palemoon-32.5.0.win64.installer.exe
