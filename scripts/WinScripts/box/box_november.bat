6aecc33c25311e9cc9879e11a1626bf7cae0849e
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-15 20:11:14 +0100

    scripts: another hotfix to box switch scripts

--- scripts/WinScripts/box/box_november.bat
+++ scripts/WinScripts/box/box_november.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start /unix /data/data/com.termux/files/usr/glibc/opt/WinScripts/box/update-box.sh October
+start /unix /data/data/com.termux/files/usr/glibc/opt/WinScripts/box/update-box.sh November
