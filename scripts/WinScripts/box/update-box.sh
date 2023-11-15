7c010fd62ec6dc53069254bb85196360fac39dfb
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-11-15 20:00:00 +0100

    scripts: hotfix to script to switch box versions

--- scripts/WinScripts/box/update-box.sh
+++ scripts/WinScripts/box/update-box.sh
@@ -7,10 +7,10 @@ cachedir=$HOME/.local/cache
 
 mkdir -p $cachedir
 
-[[ ! -f $cachedir/box64_$1.tar.xz ]] && wget https://github.com/Pipetto-crypto/box86_box64_debs/raw/master/box64/box64_$1.tar.xz
-[[ ! -f $cachedir/box86_$1.tar.xz ]] && https://github.com/Pipetto-crypto/box86_box64_debs/raw/master/box86/box86_$1.tar.xz
+[[ ! -f $cachedir/box64_$1.tar.xz ]] && wget https://github.com/Pipetto-crypto/box86_box64_debs/raw/master/box64/box64_$1.tar.xz -P $cachedir
+[[ ! -f $cachedir/box86_$1.tar.xz ]] && wget https://github.com/Pipetto-crypto/box86_box64_debs/raw/master/box86/box86_$1.tar.xz -P $cachedir
 
-tar -xvf $cachedir/box64_$1.tar.xz -C $PREFIX/glibc/bin
-tar -xvf $cachedir/box86_$1.tar.xz -C $PREFIX/glibc/bin
+tar -xf $cachedir/box64_$1.tar.xz -C $PREFIX/glibc/bin
+tar -xf $cachedir/box86_$1.tar.xz -C $PREFIX/glibc/bin
 sleep 2
 $PREFIX/glibc/opt/WinScripts/restart.sh
