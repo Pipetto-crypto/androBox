1e7c5249e08b47399e88d79e3fab6deb9eb51e0b
Pipetto-crypto <Pipetto-crypto@gmail.com>
2023-12-07 11:52:38 +0100

    scripts: Only load ProcessGovernor on startup to avoid filling up logs

--- scripts/WinScripts/startup.bat
+++ scripts/WinScripts/startup.bat
@@ -2,6 +2,5 @@
 
 start /B F:\ProcessLasso\ProcessGovernor.exe
 start /B F:\7zip\7zFM.exe E:
-start /B F:\ProcessLasso\ProcessLasso.exe
 start /unix /data/data/com.termux/files/usr/glibc/opt/WinScripts/startup.sh
 start /B F:\IB\ib.exe
