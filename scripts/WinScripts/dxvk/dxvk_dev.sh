c07c3c585ade79af039a8b5a7792fe829ca0b467
Pipetto-crypto <Pipetto-crypto@gmail.com>
2024-01-01 12:51:54 +0100

    scripts: Add DXVK-dev

---
+++ scripts/WinScripts/dxvk/dxvk_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cachedir=$HOME/.local/cache
+mkdir -p $cachedir
+
+wget https://github.com/Pipetto-crypto/androBox/releases/download/dxvk-dev/dxvk-dev.tar.xz -P $cachedir 
+tar -xvf $cachedir/dxvk-dev.tar.xz -C $HOME
+mv $HOME/dxvk-dev/x64/* $HOME/.wine/drive_c/windows/system32
+mv $HOME/dxvk-dev/x32/* $HOME/.wine/drive_c/windows/syswow64
+rm -rf $HOME/dxvk-dev $cachedir/dxvk-dev.tar.xz
