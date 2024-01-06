0c51e1921176c22f1ddcb7433f7715c975170867
Pipetto-crypto <Pipetto-crypto@gmail.com>
2024-01-06 07:56:59 +0100

    scripts: Add Turnip January builds

---
+++ scripts/WinScripts/mesa/turnip_aarch64_01.05.24.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start /unix /data/data/com.termux/files/usr/bin/mesaswitch aarch64 turnip 01.05.24
---
+++ scripts/WinScripts/mesa/turnip_armhf_01.05.24.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start /unix /data/data/com.termux/files/usr/bin/mesaswitch armhf turnip 01.05.24
