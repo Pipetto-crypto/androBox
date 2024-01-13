bbc0aa4168d65006cb138d08d8d20f09197e15d9
Pipetto-crypto <Pipetto-crypto@gmail.com>
2024-01-13 07:50:21 +0100

    scripts: Fix ProcessLasso affinity in wine 9.0

--- scripts/WinScripts/wine/switch_wine.sh
+++ scripts/WinScripts/wine/switch_wine.sh
@@ -38,8 +38,5 @@ rm -rf $HOME/.wine
 WINEESYNC=0 WINEDLLOVERRIDES="mscoree=" wine wineboot
 sleep 3
 pfxupdate
-user="$(ls $HOME/.wine/drive_c/users | grep -e '^u')"
-mkdir -p $HOME/.wine/drive_c/users/$user/AppData/Local/ProcessLasso/config
-cp -r $PREFIX/glibc/opt/Programs/ProcessLasso/prolasso.ini $HOME/.wine/drive_c/users/u*/AppData/Local/ProcessLasso/config
 sleep 3
 $PREFIX/glibc/opt/WinScripts/restart.sh
