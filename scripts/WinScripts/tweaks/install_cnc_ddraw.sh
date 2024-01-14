b79a6e3bd12ba546aef5d8472a8768840e1689c6
Pipetto-crypto <Pipetto-crypto@gmail.com>
2024-01-14 08:50:28 +0100

    scripts: fix cnc_ddraw installer script

--- scripts/WinScripts/tweaks/install_cnc_ddraw.sh
+++ scripts/WinScripts/tweaks/install_cnc_ddraw.sh
@@ -7,5 +7,5 @@ cachedir=$HOME/.local/cache
 mkdir -p $cachedir
 
 [[ ! -f $cachedir/cnc-ddraw.zip ]] && wget https://github.com/FunkyFr3sh/cnc-ddraw/releases/download/v6.0.0.0/cnc-ddraw.zip -P $cachedir
-unzip $cachedir/cnc-ddraw.zip -d $HOME/.wine/drive_c/windows/syswow64
+unzip -o $cachedir/cnc-ddraw.zip -d $HOME/.wine/drive_c/windows/syswow64
 
