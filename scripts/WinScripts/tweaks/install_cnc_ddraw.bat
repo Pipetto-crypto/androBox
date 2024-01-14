ba8d2558da50d23ade38905c7568ec9011759e11
Pipetto-crypto <Pipetto-crypto@gmail.com>
2024-01-14 08:35:37 +0100

    scripts: replace cnc_ddraw winetricks installation with a custom one

--- scripts/WinScripts/tweaks/install_cnc_ddraw.bat
+++ scripts/WinScripts/tweaks/install_cnc_ddraw.bat
@@ -1,3 +1,5 @@
 @echo off
 
+reg add HKEY_CURRENT_USER\Software\Wine /v DllOverrides /f
+reg add HKEY_CURRENT_USER\Software\Wine\DllOverrides /v ddraw /t REG_SZ /d native,builtin /f
 start /unix /data/data/com.termux/files/usr/glibc/opt/WinScripts/tweaks/install_cnc_ddraw.sh
--- scripts/WinScripts/tweaks/install_cnc_ddraw.sh
+++ scripts/WinScripts/tweaks/install_cnc_ddraw.sh
@@ -3,5 +3,9 @@
 export PATH=/data/data/com.termux/files/usr/bin
 unset LD_LIBRARY_PATH
 
-xterm -e $PREFIX/glibc/opt/WinScripts/winetricks.sh cnc_ddraw
-$PREFIX/glibc/opt/WinScripts/restart.sh
+cachedir=$HOME/.local/cache
+mkdir -p $cachedir
+
+[[ ! -f $cachedir/cnc-ddraw.zip ]] && wget https://github.com/FunkyFr3sh/cnc-ddraw/releases/download/v6.0.0.0/cnc-ddraw.zip -P $cachedir
+unzip $cachedir/cnc-ddraw.zip -d $HOME/.wine/drive_c/windows/syswow64
+
